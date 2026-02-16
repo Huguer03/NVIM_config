79b2208f413ad2b8457602fa71b6dc195775023f
Huguer03 <hgongon03@gmail.com>
2026-02-16 11:28:05 +0100

    Añadida una terminal (Toggleterm.nvim)

--- init.lua
+++ init.lua
@@ -48,6 +48,18 @@ vim.opt.termguicolors = true      -- Para que Treesitter y los temas se vean bie
 -- Atajos de teclado (Keymaps)
 vim.keymap.set("i", "jk", "<Esc>", { desc = "Salir del modo insertar con jk" })
 
+function _G.set_terminal_keymaps()
+  local opts = {buffer = 0}
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts) -- Salir de modo terminal con Esc
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts) -- Moverse a la izquierda
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts) -- Moverse abajo
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts) -- Moverse arriba
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts) -- Moverse a la derecha
+end
+
+-- Solo aplica estos mapeos cuando se abre la terminal
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
+
 local ls = require("luasnip")
 
 -- Saltar al siguiente hueco del snippet (hacia adelante)
---
+++ lua/plugins/terminal.lua
@@ -0,0 +1,20 @@
+return {
+  'akinsho/toggleterm.nvim',
+  version = "*",
+  config = function()
+    require("toggleterm").setup({
+      size = 15, -- Altura de la terminal
+      open_mapping = [[<leader>c]], -- Tu atajo solicitado
+      hide_numbers = true, 
+      shade_terminals = true,
+      shading_factor = 2,
+      start_in_insert = true,
+      insert_mappings = true, -- Permite usar el atajo incluso en modo inserción
+      persist_size = true,
+      direction = 'horizontal', -- Se queda abajo
+      close_on_exit = true,
+      shell = vim.o.shell,
+    })
+  end
+}
+
